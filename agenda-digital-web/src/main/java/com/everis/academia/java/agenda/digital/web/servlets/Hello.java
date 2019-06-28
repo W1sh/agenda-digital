dd6bd0360e35c1bc0f8c2cc25b604257ee8e8862
Bruno Ferreira <brunoferreira.ebm@gmail.com>
2019-06-28 18:07:35 +0100

    created module web

---
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/servlets/Hello.java
@@ -0,0 +1,25 @@
+package com.everis.academia.java.agenda.digital.web.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//@WebServlet(name = "Hello", urlPatterns = "/hello")
+public class Hello extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter printwriter = resp.getWriter();
+		printwriter.println("<html>");
+		printwriter.println("<body>");
+		printwriter.println("Hello Academia Java");
+		printwriter.println("</body>");
+		printwriter.println("</html>");
+	}
+}
