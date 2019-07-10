84140c4d61b0edcd82de47392e3e3165e14f48b0
Bruno Miguel Pereira Ferreira <brunoferreira.ebm@gmail.com>
2019-07-10 09:48:00 +0100

    continuação prestador serviço

--- agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/jsf/beans/prestador/PrestadorCreateBean.java
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/jsf/beans/prestador/PrestadorCreateBean.java
@@ -1,14 +1,33 @@
 package com.everis.academia.java.agenda.digital.web.jsf.beans.prestador;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
+import org.primefaces.model.DualListModel;
+
+import com.everis.academia.java.agenda.digital.business.ITipoServicoBusiness;
+import com.everis.academia.java.agenda.digital.business.impl.TipoServicoBusiness;
 import com.everis.academia.java.agenda.digital.entidades.PrestadorServico;
+import com.everis.academia.java.agenda.digital.entidades.TipoServico;
 import com.everis.academia.java.agenda.digital.enums.TipoLogradouro;
 
 @ManagedBean(name = "prestadorCreate")
 public class PrestadorCreateBean {
 
+	private ITipoServicoBusiness business = new TipoServicoBusiness();
 	private PrestadorServico prestador = new PrestadorServico();
+	private DualListModel<TipoServico> servicosCredenciados;
+
+	@PostConstruct
+	public void init() {
+		List<TipoServico> source = (List<TipoServico>) business.read();
+		List<TipoServico> target = new ArrayList<TipoServico>();
+
+		servicosCredenciados = new DualListModel<TipoServico>(source, target);
+	}
 
 	public PrestadorServico getPrestador() {
 		return prestador;
--- agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/jsf/beans/telefone/TelefoneReadBean.java
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/jsf/beans/telefone/TelefoneReadBean.java
@@ -1,8 +1,31 @@
 package com.everis.academia.java.agenda.digital.web.jsf.beans.telefone;
 
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+
+import com.everis.academia.java.agenda.digital.business.ITelefoneBusiness;
+import com.everis.academia.java.agenda.digital.business.impl.TelefoneBusiness;
+import com.everis.academia.java.agenda.digital.entidades.Telefone;
 
 @ManagedBean(name = "telefoneRead")
 public class TelefoneReadBean {
+
+	private ITelefoneBusiness business = new TelefoneBusiness();
+	private Collection<Telefone> telefones;
+
+	@PostConstruct
+	public void init() {
+		telefones = business.read();
+	}
+
+	public Collection<Telefone> getTelefones() {
+		return telefones;
+	}
+
+	public void setTelefones(Collection<Telefone> telefones) {
+		this.telefones = telefones;
+	}
 
 }
