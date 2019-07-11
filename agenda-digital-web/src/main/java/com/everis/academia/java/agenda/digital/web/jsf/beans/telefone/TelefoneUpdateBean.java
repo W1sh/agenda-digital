f3766b29f19821f724fc11eb7138cb74f81f5dee
Bruno Miguel Pereira Ferreira <brunoferreira.ebm@gmail.com>
2019-07-11 11:10:15 +0100

    removed unused beans and pages

--- agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/jsf/beans/telefone/TelefoneUpdateBean.java
+++
@@ -1,8 +0,0 @@
-package com.everis.academia.java.agenda.digital.web.jsf.beans.telefone;
-
-import javax.faces.bean.ManagedBean;
-
-@ManagedBean(name = "telefoneUpdate")
-public class TelefoneUpdateBean {
-
-}
